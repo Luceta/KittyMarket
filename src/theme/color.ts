4f7a9e4a0140f202e9f300c60dfabdad421cbbd9
Luceta <57941164+Luceta@users.noreply.github.com>
2022-05-08 17:24:33 +0900

    🎉 Initial settings

---
+++ src/theme/color.ts
@@ -0,0 +1,21 @@
+type Colors = {
+  WHITE_COLOR: string;
+  YELLOW_COLOR: string;
+  BROWN_COLOR: string;
+  BLACK_COLOR: string;
+  DARK_GREY: string;
+  LIGHT_GREY: string;
+  MAIN_BG: string;
+};
+
+const colors: Colors = {
+  WHITE_COLOR: "#ffff",
+  YELLOW_COLOR: "#e0a800",
+  BROWN_COLOR: "#937456",
+  BLACK_COLOR: "#353b48",
+  LIGHT_GREY: "#dfe0df",
+  DARK_GREY: "#767676",
+  MAIN_BG: "#c98128",
+};
+
+export default colors;
---
+++ src/theme/index.ts
@@ -0,0 +1,26 @@
+import colors from "./color";
+
+interface Theme {
+  mainBgColor: string;
+  textColor: string;
+  accentColor: string;
+}
+
+const lightTheme: Theme = {
+  mainBgColor: colors.WHITE_COLOR,
+  textColor: "#1e272e",
+  accentColor: colors.YELLOW_COLOR,
+};
+const darkTheme: Theme = {
+  mainBgColor: colors.BLACK_COLOR,
+  textColor: "#d2dae2",
+  accentColor: colors.YELLOW_COLOR,
+};
+
+const theme = {
+  colors,
+  lightTheme,
+  darkTheme,
+};
+
+export default theme;
